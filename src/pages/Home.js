361b0b9d3f04096d300fc417f6ddb6ff30ceb558
Kjuh28 <kjuh28@gmail.com>
2024-01-19 14:38:18 -0300

    Criado componente LinkButton e alterado a page Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,15 @@
+import LinkButton from '../Components/layout/LinkButton'
+import savings from '../img/savings.svg'
+import styles from './Home.module.css'
+
 function Home() {
     return (
-        <p>Home</p>
+        <section className={styles.home_container}>
+            <h1>Bem-vindo ao <span>Costs</span> </h1>
+            <p>Comece a genenciar os seus projetos agora mesmo!</p>
+            <LinkButton to='/Projetos'  text='Criar Projeto'/>
+            <img src={savings} alt="Costs"></img>
+        </section>
     )
 }
 export default Home
