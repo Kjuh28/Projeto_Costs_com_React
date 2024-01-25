ac09a6efb99860067c04f59d2d88dd4b08c0a2ae
Kjuh28 <kjuh28@gmail.com>
2024-01-25 17:15:13 -0300

    Criado arquivo db.json e criado chamado do db para a lista de categorias do formulario pelo backend

--- src/Components/projects/ProjectForm.js
+++ src/Components/projects/ProjectForm.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 import styles from './ProjectForm.module.css'
 
 import Input from '../form/Input'
@@ -5,6 +7,20 @@ import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 
 function ProjectForm ({btnText}){
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() =>{
+        fetch('http://localhost:5000/categories', {
+            method:'GET',
+            headers:{
+                'Content-Type':'application/json',
+            },
+        }).then((resp) => resp.json()
+            ).then((data) => { setCategories(data) }
+            ).catch((err) => console.log(err))
+        }, [])
+
     return(
         <form className={styles.form}>
             <Input 
@@ -21,7 +37,8 @@ function ProjectForm ({btnText}){
             />
             <Select 
                 name='category_id'
-                text='Categoria'            
+                text='Categoria'
+                options={categories}
             />
             <SubmitButton text={btnText}/>
 
