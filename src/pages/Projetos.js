f15d2d8b592feae00d96766c923caff821150600
unknown <kjuh28@gmail.com>
2024-02-26 16:07:33 -0300

    Criado estrutura inicial da pagina projetos

--- src/pages/Projetos.js
+++ src/pages/Projetos.js
@@ -1,5 +1,10 @@
 import { useLocation } from "react-router-dom"
 import Message from "../Components/layout/Message"
+import styles from './Projetos.module.css'
+
+import LinkButton from '../Components/layout/LinkButton'
+
+import Container from '../Components/layout/Container'
 
 function Projetos(){
     const location = useLocation()
@@ -9,9 +14,18 @@ function Projetos(){
     }
 
     return(
-      <div>
-        <h1>Mensagem</h1>
-        {message && <Message type="success" msg={message} />}
+      <div className={styles.project_container}>
+        <div className={styles.tittle_container}>
+          <h1>Meus Projetos</h1>
+          <LinkButton to="/NovoProjeto" text="Criar Projeto"/>
+        </div>
+        <div>
+          {/* <h1>Mensagem</h1> */}
+          {message && <Message type="success" msg={message} />}
+          <Container customClass=".start">
+            <p>Projetos...</p>
+          </Container>
+        </div>
       </div>
     )
 }
